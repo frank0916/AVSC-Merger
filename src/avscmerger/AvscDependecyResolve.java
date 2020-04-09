53a9282744e7edd6c07d4045d3a6bea865eceea6
frank <frank.he@igt.com>
2020-04-09 11:00:08 -0700

    add namespace to search key to fix name conflicts when name is same

--- src/avscmerger/AvscDependecyResolve.java
+++ src/avscmerger/AvscDependecyResolve.java
@@ -13,116 +13,164 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class AvscDependecyResolve {
-    char FP = File.separatorChar;
+	Pattern pattern = Pattern.compile("\"[a-zA-Z][a-zA-Z0-9_.]*[a-zA-Z0-9]\"");
+	char FS = File.separatorChar;
 
-    String source, target;
+	String source, target;
 
-    public AvscDependecyResolve(String source, String target) {
-	this.source = source;
-	this.target = target;
-    }
-
-    private Map<String, String> unsolved = new HashMap<>();
-
-    /**
-     * Register a unsolved file
-     * 
-     * @param file
-     * @throws Exception
-     */
-    public void register(File file) {
-	try {
-	    String s = Files.lines(Paths.get(file.getPath()))
-		    .collect(Collectors.joining());
-	    int start = s.indexOf("{") + 1;
-	    int end = s.indexOf(",", start);
-	    String name = s.substring(start, end);
-	    name = name.split(":")[1].replace("\"", "").trim();
-	    start = s.indexOf("namespace", end);
-	    end = s.indexOf(",", start);
-	    String namespace = s.substring(start, end);
-	    namespace = namespace.split(":")[1].replace("\"", "").trim();
-	    name = namespace + "." + name;
-	    unsolved.put(name, s);
-	} catch (Throwable t) {
-	    t.printStackTrace();
-	}
-    }
-
-    private Map<String, String> resolved = new HashMap<>();
-
-    Pattern p = Pattern.compile("\"[a-zA-Z][a-zA-Z0-9_]*\"");
-
-    public String resolve(String key, String text) {
-	String schema = resolved.get(key);
-	if (schema != null) {
-	    return schema;
+	public AvscDependecyResolve(String source, String target) {
+		this.source = source;
+		this.target = target;
 	}
 
-	Matcher m = p.matcher(text);
-	StringBuilder sb = new StringBuilder();
-	int index = 0;
-	while (m.find()) {
-	    int start = m.start();
-	    sb.append(text.substring(index, start));
-	    String token = m.group();
-	    String name = token.substring(1, token.length() - 1);
-	    String value = null;
-	    if (name.equals(key)) {
-		value = token;
-	    } else {
-		value = unsolved.get(name);
-		value = value == null ? token : resolve(name, value);
-	    }
-	    sb.append(value);
-	    index = m.end();
-	}
-	sb.append(text.substring(index));
-	schema = sb.toString();
+	private Map<String, String> registry = new HashMap<>();
+	private Map<String, String> resolved = new HashMap<>();
 
-	resolved.put(key, schema);
-	return schema;
-    }
-
-    public void resolveDir(File dir) {
-	File[] files = dir.listFiles();
-	for (File file : files) {
-	    register(file);
+	String getId(String text) {
+		int start = text.indexOf("{") + 1;
+		int end = text.indexOf(",", start);
+		String id = text.substring(start, end);
+		id = id.split(":")[1].replace("\"", "").trim();
+		start = text.indexOf("namespace", end);
+		end = text.indexOf(",", start);
+		String namespace = text.substring(start, end);
+		namespace = namespace.split(":")[1].replace("\"", "").trim();
+		id = namespace + "." + id;
+		return id;
 	}
-	String d = target + FP + dir.getName();
-	if (!new File(d).mkdirs()) {
-	    System.err.println(d + " directory is not created.");
-	}
-	unsolved.forEach((k, v) -> {
-	    String s = resolve(k, v);
-	    File avsc = new File(d + FP + k + ".avsc");
-	    try (Writer w = new FileWriter(avsc)) {
-		w.append(s);
-		w.close();
-	    } catch (IOException e) {
-		System.err.println("File write error : " + avsc);
-	    }
-	});
-    }
-
-    public void resolveDirs() {
-	try {
-	    File[] dirs = new File(source).listFiles(File::isDirectory);
-	    for (File dir : dirs) {
-		resolveDir(dir);
-	    }
-	} catch (Exception e) {
-	    System.err.println("Exception thrown in combineSchema " + e);
+	
+	
+	/**
+	 * Register a file
+	 * 
+	 * @param file
+	 * @throws IOException
+	 * @throws Exception
+	 */
+	void register(File f) throws IOException {
+		if (f.isFile()) {
+			String contents = Files.lines(Paths.get(f.getPath()))
+					.collect(Collectors.joining());
+			String id = getId(contents);
+			registry.put(id, contents);
+		} else if (f.isDirectory()) {
+			File[] files = f.listFiles();
+			for (File file : files) {
+				register(file);
+			}
+		}
 	}
 
-    }
-
-    public static void main(String[] args) {
-	if (args.length < 2) {
-	    System.err.println("Missing director of source or target files");
-	    System.err.println("Usage: java AvscDependecyResolve source targe");
-	    return;
+	/**
+	 * @throws IOException
+	 */
+	void register() throws IOException {
+		File f = new File(source);
+		register(f);
 	}
-	new AvscDependecyResolve(args[0], args[1]).resolveDirs();
-    }
+
+	/**
+	 * @param id
+	 * @param text
+	 * @return
+	 */
+	public String resolve(String id) {
+		String schema = resolved.get(id);
+		if (schema != null) {
+			return schema;
+		}
+
+		int index = id.lastIndexOf('.');
+		String shortId = index < 0 ? id : id.substring(index + 1);
+		String namespace = index < 0 ? null : id.substring(0, index);
+
+		String text = registry.get(id);
+		Matcher matcher = pattern.matcher(text);
+		StringBuilder sb = new StringBuilder();
+		index = 0;
+		while (matcher.find()) {
+			int start = matcher.start();
+			sb.append(text.substring(index, start));
+			String token = matcher.group();
+			String key = token.substring(1, token.length() - 1);
+			String value = null;
+
+			if (key.equals(shortId)) {
+				// ignore the first '"name", "${id}" section
+				value = token;
+			} else {
+				if (key.indexOf('.') >= 0) {
+					value = registry.get(key);
+				} else {
+					value = registry.get(namespace + '.' + key);
+				}
+				value = value == null ? token : resolve(getId(value));
+			}
+
+			sb.append(value);
+			index = matcher.end();
+		}
+		sb.append(text.substring(index));
+		schema = sb.toString();
+
+		resolved.put(id, schema);
+		return schema;
+	}
+
+	/**
+	 * 
+	 */
+	private void resolve() {
+		registry.keySet().forEach((k) -> {
+			String s = resolve(k);
+			File path = new File(target + FS + k + ".avsc");
+			try (Writer w = new FileWriter(path)) {
+				w.append(s);
+				w.close();
+			} catch (IOException e) {
+				System.err.println("File write error : " + path);
+			}
+		});
+	}
+
+	/**
+	 * @param args
+	 */
+
+	/*
+	 * [0] is source directory of AVSC files only
+	 * 
+	 * [1] is target directory to store converted AVSC files
+	 * 
+	 * all files in source directory must be in AVSC format
+	 * 
+	 * target directory should not exist or remove before starting
+	 * 
+	 */
+	public static void main(String[] args) {
+		if (args.length < 2) {
+			System.err.println("Usage: java AvscDependecyResolve source targe");
+			System.err.println("source directory of AVSC files to merge.");
+			System.err.println("target directory wiil have combined files.");
+			return;
+		}
+		AvscDependecyResolve r = new AvscDependecyResolve(args[0], args[1]);
+		File f = new File(r.target);
+		if (!f.mkdirs()) {
+			System.err.print(r.target);
+			var msg = " - failed to create directory. stopped.";
+			System.err.println(msg);
+			System.exit(1);
+		}
+
+		try {
+			r.register();
+			r.resolve();
+		} catch (IOException e) {
+			var msg = "Fail to create combined schema file(s).";
+			System.err.println(msg);
+			e.printStackTrace();
+		}
+	}
 }
+
